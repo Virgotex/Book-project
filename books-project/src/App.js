eeea125d998487baf8e54e3bb805f935e488fc9f
sammun1926 <munorusam10@gmail.com>
2023-05-04 11:14:58 +0300

    added files

--- books-project/src/App.js
+++ books-project/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import { AppProvider } from './context.';
+import './index.css';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AppProvider>
+    
+  </AppProvider>
+);
 
-export default App;
